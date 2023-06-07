9d3d2ef2992fda90b68ebdfc0766d31cc50964c2
leduyanhminer <leduyanhminer@gmail.com>
2023-06-08 01:55:14 +0700

    update bet, betButton function

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const kytu = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
 const type = ['C', 'D', 'H', 'S']
 const card = ["firstcard", "secondcard", "flopone", "floptwo", "flopthree", "turn", "river", "thirdcard", "fourthcard"]
 let check_card = []
+let my_money = 15
+let opp_money = 15
+let counter = 0
+let betButton = document.getElementById("betbutton");
+let newButtonsContainer = document.getElementById("newButtonsContainer");
+let betMoney = document.getElementById("value");
 // sinh ngau nhien la bai
 function randomCard() {
   const so = Math.floor(Math.random() * 13);
@@ -19,6 +25,8 @@ function randomCard() {
 // hand bai moi
 function newHand() {
   console.log('new')
+  newButtonsContainer.innerHTML = "";
+  counter = 0;
   check_card = [];
   for (let i = 0; i <= 8; i++) {
     randomCard();
@@ -36,10 +44,38 @@ function newHand() {
 }
 
 // lenh bet
-function bet() {
+function bet(bet_money) {
+  console.log(bet_money);
+  if(my_money<bet_money) return 0;
+  my_money -= bet_money;
+  betMoney = bet_money;
   for (let i = 5; i <= 8; ++i) {
     const card_i = document.getElementById(card[i]);
     card_i.src = `/images/${kytu[check_card[i][0]]}${type[check_card[i][1]]}.png`
     card_i.style.opacity = 1;
   }
 }
+
+betButton.addEventListener("click", function() {
+  if(counter === 0){
+    for(let i = 1; i <= 3; ++i){
+      let newButton = document.createElement("button");
+      newButton.textContent = i;
+      newButton.classList.add("detailBetButton");
+      newButton.style.top = `630px`;
+        newButton.style.left = `${1000+70*i}px`;
+      newButton.addEventListener("click", function(){
+        bet(i);
+        newButtonsContainer.innerHTML = "";
+        counter = 0;
+      });
+      newButtonsContainer.appendChild(newButton);
+
+    }
+  counter = 1;
+  }
+  else{
+    newButtonsContainer.innerHTML = "";
+    counter = 0;
+  }
+});
